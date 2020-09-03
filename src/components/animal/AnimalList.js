eb549ac5e09bef8504d1463285824870255ee8c4
Raparker88 <raparker88@gmail.com>
2020-09-03 15:35:53 -0500

    attaching dataj

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,15 +1,24 @@
 import React, { useContext } from "react"
 import { AnimalContext } from "./AnimalProvider"
+import { LocationContext } from "../location/LocationProvider"
 import { Animal } from "./Animal"
 import "./Animal.css"
+import { CustomerContext } from "../customer/CustomerProvider"
 
 export const AnimalList = () => {
     const { animals } = useContext(AnimalContext)
+    const { locations } = useContext(LocationContext)
+    const { customers } = useContext(CustomerContext)
 
     return (
         <div className="animals">
         {
-            animals.map(a => <Animal key={a.id} animal={a} />)
+            animals.map(a => {
+                const l = locations.find(l => l.id === a.locationId) || {}
+                const c = customers.find(c => c.id === a.customerId) || {}
+                return <Animal key={a.id} animal={a} location={l} customer={c} />
+        
+            })
         }
         </div>
     )
--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -2,14 +2,20 @@ import React, { useContext } from "react"
 import { EmployeeContext } from "./EmployeeProvider"
 import { Employee } from "./Employee"
 import "./Employee.css"
+import { LocationContext } from "../location/LocationProvider"
 
 export const EmployeeList = () => {
     const { employees } = useContext(EmployeeContext)
+    const { locations } = useContext(LocationContext)
 
     return (
         <div className="employees">
         {
-            employees.map(e => <Employee key={e.id} employee={e} />)
+            employees.map(e => {
+                const l = locations.find(l => l.id === e.locationId) || {}
+                return <Employee key={e.id} employee={e} location={l}/>
+            }) 
+                
         }
         </div>
     )
